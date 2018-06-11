a2b54c2114870afbc53d779223af67fa31a17a66
Alia Wilkinson <aliawilkinson@gmail.com>
2018-06-11 10:02:47 -0700

    finished php mailer, not connected yet though

---
+++ php_mailer/email_config.default.php
@@ -0,0 +1,6 @@
+<?php
+define('EMAIL_USER','<YOUR EMAIL>'); //proxy email
+define('EMAIL_USERNAME','<USER NAME>');
+define('EMAIL_PASS','<YOUR PASSWORD>');
+define('EMAIL_TO_ADDRESS', '<YOUR MORE SERIOUS EMAIL>'); //email to forward to
+?>
