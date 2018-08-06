2d444424905f1778ba25c4cab89db09f64cf40a4
Alia Wilkinson <aliawilkinson@gmail.com>
2018-08-06 15:22:50 -0700

    added space hackathon project

--- php_mailer/email_config.default.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('EMAIL_USER','<YOUR EMAIL>'); //proxy email
-define('EMAIL_USERNAME','<USER NAME>');
-define('EMAIL_PASS','<YOUR PASSWORD>');
-define('EMAIL_TO_ADDRESS', '<YOUR MORE SERIOUS EMAIL>'); //email to forward to
-?>
